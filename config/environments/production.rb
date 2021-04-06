c4620dbb75b2eced05d3269755b766a3736f5599
DaikiMaki <makidaiki9180@gmail.com>
2021-04-06 01:10:58 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<あなたのHerokuサブドメイン名>.herokuapp.com'
+  host = 'cherry-crisp-98435.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
