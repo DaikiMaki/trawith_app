6cb298cec7f4d63da28287ce6a8f914a1ecbe30a
DaikiMaki <makidaiki9180@gmail.com>
2021-04-07 09:04:37 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module TrawithApp
    
     #エラーメッセージの日本語化
     config.i18n.default_locale = :ja
+    
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
   end
 end
 
