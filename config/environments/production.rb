5ea592fa050a92a1a501623db6eed677fe6e4bb8
DaikiMaki <makidaiki9180@gmail.com>
2021-04-24 04:12:44 +0000

    production mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,14 +117,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'cherry-crisp-98435.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV["SECRET_MAIL"],
+    password:  ENV["SECRET_MAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
+  
+  
   
   # アップロードされたファイルをAWSに保存する
   config.active_storage.service = :amazon
