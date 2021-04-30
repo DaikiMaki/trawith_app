13e1e0c329c8c61407c3e5bb8b3394923db99010
DaikiMaki <makidaiki9180@gmail.com>
2021-04-30 02:14:38 +0000

    scroll

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',      '6.0.3'
+gem "jquery-rails"
 gem 'dotenv-rails'
+gem 'kaminari'
 gem 'ransack'
 gem 'seed-fu'
 gem 'aws-sdk-s3',              '1.46.0', require: false
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+//= require channels
+//= require jquery3
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery.jscroll.min.js
+//= require popper
+//= require bootstrap-sprockets
+
 import "bootstrap"
