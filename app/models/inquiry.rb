936670004165a432eeaa26a7ffa9c062b48b88c2
DaikiMaki <makidaiki9180@gmail.com>
2021-05-18 03:24:53 +0000

    inquiry validates edit

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,4 @@
 class Inquiry < ApplicationRecord
   belongs_to :user
-  validates :message, presence: true,  length: { maximum: 140 }
+  validates :message, presence: true,  length: { maximum: 1000 }
 end
